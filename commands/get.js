b511d1682e025754816d4c1bdb113e237384b097
xenoncolt <81591350+xenoncolt@users.noreply.github.com>
2021-04-04 19:58:42 +0600

    to

--- commands/get.js
+++ commands/get.js
@@ -2,9 +2,6 @@ const image_api = require('anime-images-api')
 const api = new image_api('GET')
 
 module.exports = {
-  name: 'get',
-  permissions: ["SEND_MESSAGES"],
-  description: "This is a fun command!",
   async execute(Discord, client, message, args, cmd){
     api.getHugImage().then(res => {
         console.log(res)
