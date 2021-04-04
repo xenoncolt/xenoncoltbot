9d622ad80b4bbe11f9927373998e1b09ecc9792a
xenoncolt <81591350+xenoncolt@users.noreply.github.com>
2021-04-04 18:51:18 +0600

    alhamdulillah

--- commands/fun.js
+++ commands/fun.js
@@ -1,22 +1,15 @@
-const https = require('https');
+const image_api = require('anime-images-api')
 
 module.exports = {
   name: 'fun',
   aliases: ['hug', 'kiss', 'punch', 'rand'],
   permissions: ["SEND_MESSAGES"],
   description: 'Asks a question and let the bot determine your fate :sparkler:',
-  async execute(Discord, client, message, args, cmd) {
-    let endpoints = ['hug', 'kiss', 'punch', 'rand']
-    let body = '';
-    https.get(`https://anime-api.hisoka17.repl.co/img/${endpoints[0]}`, function (res){ 
-
-    res.on('data', result => {
-        body += result;
-    })
-    .on('end', function(){
-        let data = JSON.parse(body)
-        console.log(data.url?data.url:null)
-    })
-    }) 
+  async execute(Discord, client, message, args, cmd) { 
+    if (m.content.startsWith('test')){
+      api.getHugImage().then(res => {
+      m.channel.send(res.image)
+      })
+    }
   },
 };
