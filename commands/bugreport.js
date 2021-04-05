78cbb0dd8484eac711e3a9c338b8336dd5ae2337
xenoncolt <81591350+xenoncolt@users.noreply.github.com>
2021-04-05 18:15:44 +0600

    oiek

---
+++ commands/bugreport.js
@@ -0,0 +1,27 @@
+const { execute } = require("./say");
+
+module.exports = {
+    name: "bugreport",
+    aliases: ['bug', 'reportbug', 'problem'],
+    description: 'let users report bugs',
+    async execute(message, args, client, Discord){
+        //the channel you want the bug-reports to be send to
+        const channel = client.channels.cache.get('828281141240659998')
+
+         //look if there is a bug specified
+        const query = args.join(' ');
+        if(!query) return message.reply('Please specify the bug')
+        
+         //create an embed for the bug report
+        const reportEmbed = new Discord.MessageEmbed()
+        .setTitle('New Bug!')
+        .addField('Author', message.author.toString(), true)
+        .addField('Guild', message.guild.name, true)
+        .addField('Report', query)
+        .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
+        .setTimestamp()
+        channel.send(reportEmbed);
+        
+        message.channel.send("**Bug report has been sent!**")
+    }
+}
