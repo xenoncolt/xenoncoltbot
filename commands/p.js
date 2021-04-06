a7abd648b17a175a7b102b34b6aef901d4f4d0c4
xenoncolt <81591350+xenoncolt@users.noreply.github.com>
2021-04-07 01:47:34 +0600

    Update p.js

--- commands/p.js
+++ commands/p.js
@@ -53,7 +53,9 @@ module.exports = {
                     voice_channel: voice_channel,
                     text_channel: message.channel,
                     connection: null,
-                    songs: []
+                    songs: [],
+                    playing: true,
+                    loop: false
                 }
                 
                 
@@ -79,7 +81,8 @@ module.exports = {
         else if(cmd === 'skip') skip_song(message, server_queue);
         else if(cmd === 'stop') stop_song(message, server_queue);
         else if(cmd === 'pause') pause_song(message, server_queue);
-        else if(cmd === 'resume') resume_song(message, server_queue);
+        else if(cmd === 'play') resume_song(message, server_queue);
+        else if(cmd === 'loop') loop_song(message, server_queue);
     }
     
 }
@@ -96,7 +99,7 @@ const video_player = async (guild, song) => {
     const stream = ytdl(song.url, { filter: 'audioonly' });
     song_queue.connection.play(stream, { seek: 0, volume: 1 })
     .on('finish', () => {
-        song_queue.songs.shift();
+        if (!server_queue.loop) song_queue.songs.shift();
         video_player(guild, song_queue.songs[0]);
     });
     await song_queue.text_channel.send(`🎶 :speaking_head: Now Xenon singing \`${song.title}\``)
@@ -126,6 +129,15 @@ const pause_song = (message, server_queue) => {
 const resume_song = (message, server_queue) => {
     if(!server_queue.connection.dispatcher.paused) return message.channel.send("Song isn't paused!");
     server_queue.connection.dispatcher.resume();
-    message.channel.send("resumed the song!");
+    message.channel.send("Played the song!");
       
+}
+
+const loop_song = (message, server_queue) => {
+    if (!message.member.voice.channel) return message.channel.send('You need to be in a **channel** to loop music!');
+    if(!server_queue) return message.channel.send(`There are no songs in queue 😔`); 
+
+    server_queue.loop = !server_queue.loop
+
+    return message.channel.send(`I have now ${server_queue.loop ? `**Enabled**` : `**Disabled**`} loop.`)
 }
