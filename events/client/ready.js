5a9f6ff3ff8ea7de9ef29479e45b3b8c430f062e
xenoncolt <81591350+xenoncolt@users.noreply.github.com>
2021-05-22 11:52:59 +0600

    dfffdad

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,12 @@
 module.exports = ()=>{
     console.log('Sundai command is online!');
+    setInterval(() => {
+
+        stateswitch = !stateswitch;
+
+        if (stateswitch)
+            client.user.setActivity(`${PREFIX}help | TEAM GX (Xavier)`, { type: "LISTENING" });
+        else
+            client.user.setActivity(`${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} Users | ${client.guilds.cache.size} Servers`, { type: "LISTENING" });
+    }, (5000));
 }
--- index.js
+++ index.js
@@ -10,17 +10,7 @@ client.events = new Discord.Collection();
 
 client.once('ready', () => {
     console.log('sundai command is online!');
-    setInterval(() => {
-
-        stateswitch = !stateswitch;
-
-        if (stateswitch)
-            client.user.setActivity(`${PREFIX}help | TEAM GX (Xavier)`, { type: "LISTENING" });
-        else
-            client.user.setActivity(`${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} Users | ${client.guilds.cache.size} Servers`, { type: "LISTENING" });
-    }, (5000));
 })
-
 
 ['command_handler', 'event_handler'].forEach(handler =>{
     require(`./handlers/${handler}`)(Discord, client);
